a4ea5ba9e02858bf34c3dd5ec0daaaf05953928e
Isabel Kloumann <imk36@cornell.edu>
2016-12-20 12:29:38 -0500

    Style Center Display & Add Examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
 	return (
 		<div>
-			<h3>Examples</h3>
-			<p>Welcome to the Examples Page </p>
+			<h1 className="text-center">Examples</h1>
+			<p>Here are a few example locations to try out: </p>
+			<ol>
+				<li>
+					<Link to='/?location=Philidelphia'>Philidelphia, PA</Link>
+				</li>
+				<li>
+					<Link to='/?location=Cape Town'> Cape Town, South Africa </Link>
+				</li>
+			</ol>
 		</div>
 		)
 }
